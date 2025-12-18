20a733347e8e4ed32fb5e2092635cf9e21de3caf
Patrick Fanella <61631520+PatrickFanella@users.noreply.github.com>
2025-12-18 15:25:19 -0600

    Revert "Add channel management interface with member roles and permissions (#…" (#748)
    This reverts commit fff09896edf3893cf1ce38d7f73db606dc53483f.

--- frontend/src/components/chat/index.ts
+++ frontend/src/components/chat/index.ts
@@ -12,8 +12,3 @@ export { MessageList } from './MessageList';
 export { EmojiPicker } from './EmojiPicker';
 export { TypingIndicator } from './TypingIndicator';
 export { LinkPreview } from './LinkPreview';
-export { CreateChannelModal } from './CreateChannelModal';
-export { ChannelMemberList } from './ChannelMemberList';
-export { InviteMembersModal } from './InviteMembersModal';
-export { ChannelSettingsPage } from './ChannelSettingsPage';
-export { ChannelDiscoveryPage } from './ChannelDiscoveryPage';
